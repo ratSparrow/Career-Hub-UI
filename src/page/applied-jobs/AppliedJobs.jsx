fa07bab472b83f4d5dcc00d402c8d6ce1d0784ae
RafiulAlam <mrafiul.alam7@gmail.com>
2024-08-17 22:39:49 +0600

    updated

--- src/page/applied-jobs/AppliedJobs.jsx
+++ src/page/applied-jobs/AppliedJobs.jsx
@@ -1,11 +1,13 @@
+import { useEffect, useState } from 'react';
+
 import { getStoredJobApplication } from '../../utility/localstorage';
-import { useEffect } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 AppliedJobs.propTypes = {};
 
 function AppliedJobs() {
   const jobs = useLoaderData();
+  const [appliedJobs, setAppliedJobs] = useState([]);
   useEffect(() => {
     const storedJobIds = getStoredJobApplication();
     if (jobs.length > 0) {
@@ -20,9 +22,37 @@ function AppliedJobs() {
         }
       }
       console.log(jobsApplied);
+      setAppliedJobs(jobsApplied);
     }
   }, []);
-  return <div>Applied Jobs</div>;
+  return (
+    <div>
+      <h2>Applied Jobs</h2>
+      <details className="dropdown mb-32">
+        <summary className="m-1 btn">open or close</summary>
+        <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+          <li>
+            <a>All</a>
+          </li>
+          <li>
+            <a>Remote</a>
+          </li>
+          <li>
+            <a>onsite</a>
+          </li>
+        </ul>
+      </details>
+      <ul>
+        {appliedJobs.map(job => (
+          <li key={job.id}>
+            <span>
+              {job.job_title} {job.company_name}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default AppliedJobs;
